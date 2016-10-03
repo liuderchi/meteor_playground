1807000b8dfa88b2afc1737f08bd4fbfd0254d4c
liuderchi <liuderchi@gmail.com>
2016-10-04 00:33:55 +0800

    [feat] lec 12 create/export components ImageList
    - create components: image_list.js

---
+++ myapp/client/components/image_list.js
@@ -0,0 +1,16 @@
+// create image list component
+
+// import react
+import React from 'react';
+
+// create component
+const ImageList = () => {  // NOTE component is a function obj
+  return (
+    <ul>
+      <li>Image Detail</li>
+    </ul>
+  );  // NOTE use braces to wrap JSX
+};
+
+// export created component
+export default ImageList;
