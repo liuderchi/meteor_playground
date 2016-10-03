75320b2536da58499541654b218e2dfc99abf1f4
liuderchi <liuderchi@gmail.com>
2016-10-04 00:49:20 +0800

    [feat] lec 13 adding component to app.js
    - remove redundant dom element in main.html
    - import ImageList in main.js, use it as <ImageList />

--- myapp/client/main.js
+++ myapp/client/main.js
@@ -3,17 +3,19 @@
 // Import the React library
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ImageList from './components/image_list';
 
 // Create a component
 const App = () => {
   return (
     <div>
-      React App #2
+      <ImageList />
     </div>
   );
 };
 
-// Render this component to the screen
+// Render this root App component to the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
+  // NOTE ReactDOM is usually used one time
 });
