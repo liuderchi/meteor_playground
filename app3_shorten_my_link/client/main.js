923f534145dbc30e4063dd26f6b50f4f5949e203
liuderchi <liuderchi@gmail.com>
2016-11-03 23:57:45 +0800

    [fix] fix lec 60: rename create_link.js to link_create.js

--- app3_shorten_my_link/client/main.js
+++ app3_shorten_my_link/client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/header';
-import LinkCreate from './components/create_link';
+import LinkCreate from './components/link_create';
 
 const App = () => {
   return (
