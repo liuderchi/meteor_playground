36935ad7ec90d9c6b62ca712eb5047cb04c5a92f
liuderchi <liuderchi@gmail.com>
2016-11-13 00:45:07 +0800

    [feat] lec 71: Add middleware for server
    - import meteor WebApp
    - npm install --save connect-route
    - use ConnectRoute to add middleware to WebApp

--- app3_shorten_my_link/server/main.js
+++ app3_shorten_my_link/server/main.js
@@ -1,8 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
 
 Meteor.startup(() => {
   Meteor.publish('links', function(){
     return Links.find({});
   });
 });
+
+
+const middleware = ConnectRoute(function(router){
+  router.get('/:token', (req) => console.log(req.params.token));
+  // NOTE if incoming req match the string, call this middleware
+
+  // localhost:3000/ NOT Match
+  // localhost:3000/foo/bar NOT Match
+  // localhost:3000/abcd Match
+});
+
+WebApp.connectHandlers.use(middleware);
