999e5cebea6c8fb4eccc621b39055bd5ec34a29f
liuderchi <liuderchi@gmail.com>
2016-12-08 18:01:50 +0800

    [feat] lec 84: insert empty data to Bins collection
    - register meteor method to create bin
    - use meteor method: this.userId

--- app4_markbin/imports/collections/bins.js
+++ app4_markbin/imports/collections/bins.js
@@ -1,3 +1,15 @@
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+  'bins.insert': function() {   // NOTE NO fat arrow to keep this.userId clean
+    return Bins.insert({
+      createdAt: new Date(),
+      content: '',
+      sharedWith: [],
+      ownerId: this.userId
+      // NOTE this.userId is meteor method RPC API
+    });
+  }
+});
+
 export const Bins = new Mongo.Collection('bins');
