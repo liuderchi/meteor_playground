43dbc1db08736a6731b8b4c64ac6e804a7d4b637
liuderchi <liuderchi@gmail.com>
2016-12-17 14:39:09 +0800

    [feat] lec 86: add bin publishment and meteor remove autopublish
    - NOTE: DO NOT use fat arrow function

--- app4_markbin/server/main.js
+++ app4_markbin/server/main.js
@@ -3,4 +3,9 @@ import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  Meteor.publish('bins', function() {  // NOTE DO NOT use fat arrow fcn
+    return Bins.find({ownerId: this.userId});
+    // meteor method: this.userId  // https://docs.meteor.com/api/methods.html
+  });
 });
