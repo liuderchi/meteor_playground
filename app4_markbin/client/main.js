12d3702e5404dc52b55074ea921e361cc853ab21
liuderchi <liuderchi@gmail.com>
2016-12-18 22:43:53 +0800

    [feat] lec 91 demo router
    - create binsMain component for url is /bins/binId
    - main.js: apply nested <Route> to demo url-component mapping
      - temporarily render <BinsList> when url is /list (should be /)
      - render <BinsMain> when ursl is /bins (sould be /bins/binId)
    - use props.children to display dynamic component in App

--- app4_markbin/client/main.js
+++ app4_markbin/client/main.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import BinsList from './components/bins/bins_list';
+import BinsMain from './components/bins/bins_main';
 import { Bins } from '../imports/collections/bins';
 
 const routes = (
   <Router history={browserHistory}>
     {/*NOTE jsx component as a Router*/}
     <Route path="/" component={App}>
+      {/*TODO render BinList when path is / */}
+      <Route path="list" component={BinsList}></Route>
+
+      {/*TODO render BinMain when path is /bins/:binId */}
+      <Route path="bins" component={BinsMain}></Route>
     </Route>
   </Router>
 );
