a845941a3fa5c889637f779719ead7b99b7d69de
liuderchi <liuderchi@gmail.com>
2016-12-19 18:42:59 +0800

    [feat] lec 94: use <Link> to create dynamic link for different bin
    - <Link to="...">

--- app4_markbin/client/components/bins/bins_main.js
+++ app4_markbin/client/components/bins/bins_main.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class BinsMain extends Component {
   render() {
-    console.log(this.props.params.binId);
     return (
       <div>BinsMain</div>
     );
