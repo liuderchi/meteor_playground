ff366c85d74c525f4b5be0391d8013bf9d05550f
liuderchi <liuderchi@gmail.com>
2016-12-21 00:02:19 +0800

    [feat] lec 97: subscribe corresponding bin by binId from url
    - access url in createContainer Cb via arg 'props'
    - subscribe particular bin via Bin.findOne(props.params.binId)
    - in render(), access bin via this.props.bin

--- app4_markbin/client/components/bins/bins_main.js
+++ app4_markbin/client/components/bins/bins_main.js
@@ -4,13 +4,18 @@ import { Bins } from '../../../imports/collections/bins';
 
 class BinsMain extends Component {
   render() {
+    console.log(this.props.bin);
     return (
       <div>BinsMain</div>
     );
   }
 }
 
-export default createContainer(() => {
-  // TODO Meteor.subscribe(...)
+export default createContainer((props) => {
+  // NOTE access url like accessing this.props.params.binId in render() scope
 
+  const { binId } = props.params;  // NOTE props arg is coming from BinsMain Component
+  Meteor.subscribe('bins');  // subscribe even we did it in BinsList
+
+  return { bin: Bins.findOne(binId) };  // access it by props.bin in reder() scope
 }, BinsMain);
