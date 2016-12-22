ee4fb48211e61929acf94a4f8086fc1168fa45fa
liuderchi <liuderchi@gmail.com>
2016-12-22 23:36:17 +0800

    [feat] lec 99: add <CodeMirror> component
    - npm install --save react-codemirror
    - meteor add sgslo:cmstyle

--- app4_markbin/client/components/bins/bins_editor.js
+++ app4_markbin/client/components/bins/bins_editor.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';  // run this script
 
 class BinsEditor extends Component {
   render() {
-    console.log(this.props.bin);
     return (
-      <div>Bins Editor</div>
+      <div className="col-xs-8">
+        <h5>Input</h5>
+        <CodeMirror options={{ mode: 'markdown', lineNumbers: true }}/>
+        {/*NOTE pass object {} into options*/}
+      </div>
     );
   }
 }
