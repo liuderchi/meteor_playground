90b2039d766316d54192e341cbb680011ebc7288
liuderchi <liuderchi@gmail.com>
2016-12-22 20:16:12 +0800

    [feat] lec 98: add <BinsEditor> component
    - pass this.props.bin into <BinsEditor bin={...}>
    - access bin also via this.props.bin

---
+++ app4_markbin/client/components/bins/bins_editor.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BinsEditor extends Component {
+  render() {
+    console.log(this.props.bin);
+    return (
+      <div>Bins Editor</div>
+    );
+  }
+}
+
+export default BinsEditor;
