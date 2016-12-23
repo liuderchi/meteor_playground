9a45be3b8aee08f1cc7e0a4527176528c73d40aa
liuderchi <liuderchi@gmail.com>
2016-12-23 22:31:29 +0800

    [feat] lec 101: update bin when content is changed
    - also bind CodeMirror with value attr
    - add reference undefined handling in <BinsMain>

--- app4_markbin/client/components/bins/bins_editor.js
+++ app4_markbin/client/components/bins/bins_editor.js
@@ -5,15 +5,14 @@ import 'codemirror/mode/markdown/markdown';  // run this script
 class BinsEditor extends Component {
   onEditorChange(content) {
     // NOTE content is not event object
-    console.log(content);
-
-    // TODO update bin with content
+    Meteor.call('bins.update', this.props.bin, content);
   }
   render() {
     return (
       <div className="col-xs-8">
         <h5>Input</h5>
         <CodeMirror
+          value={this.props.bin.content}
           onChange={this.onEditorChange.bind(this)}
           options={{ mode: 'markdown', lineNumbers: true }}/>
         {/*NOTE pass object {} into options*/}
