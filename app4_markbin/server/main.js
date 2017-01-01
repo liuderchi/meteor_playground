ca61663b0d3ab7d8063f6b06d74012ac22b0d68e
liuderchi <liuderchi@gmail.com>
2017-01-01 22:06:10 +0800

    [feat] lec 108: create separated sharedBins publication
    - use Meteor.users to get user email

--- app4_markbin/server/main.js
+++ app4_markbin/server/main.js
@@ -8,4 +8,16 @@ Meteor.startup(() => {
     return Bins.find({ownerId: this.userId});
     // meteor method: this.userId  // https://docs.meteor.com/api/methods.html
   });
+
+  Meteor.publish('sharedBins', function() {
+    const user = Meteor.users.findOne(this.userId);
+    // return current user via Meteor method
+
+    if (!user) { return; }
+    const email = user.emails[0].address;
+
+    return Bins.find({
+      sharedWith: { $elemMatch: { $eq: email } }
+    });
+  });
 });
